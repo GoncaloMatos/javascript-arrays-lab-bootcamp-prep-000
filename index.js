396bc514dd88a9af245c3b27a75006fbcd65c6dc
Gonçalo Matos <matos98.ff@gmail.com>
2018-09-04 10:09:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const a = ["Milo", "Otis", "Garfield"];
+
 function appendKitten (name) {
   const b =  [...a, name];
   return b;
 }
 
 function prependKitten (name) {
-  const c = []
+  const c = [name, ...a];
+  return c;
 }
